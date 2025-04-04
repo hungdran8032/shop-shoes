dd9fa087eedd3bc3f4087ed80f8068ddba9d9f9d
Tanfuu Lu <lutanfuu@gmail.com>
2025-04-04 22:59:54 +0700

    remove brand from Phuc

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,11 +13,3 @@ Route::prefix('v1/products')->group(function () {
     Route::put('/{id}', [ProductController::class, 'update']);
     Route::delete('/{id}', [ProductController::class, 'destroy']);
 });
-//api/v1/brands
-Route::prefix('v1/brands')->group(function () {
-    Route::get('/', [BrandController::class, 'index']);
-    Route::post('/', [BrandController::class, 'store']);
-    Route::get('/{id}', [BrandController::class, 'show']);
-    Route::put('/{id}', [BrandController::class, 'update']);
-    Route::delete('/{id}', [BrandController::class, 'destroy']);
-});
