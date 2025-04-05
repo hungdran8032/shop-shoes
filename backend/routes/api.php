3f8e871b53f7c231eb080e7a91e036aa63bf4200
hairlee462 <hairlee462@gmail.com>
2025-04-05 13:51:07 +0700

    crud_colors

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TestController;
 use App\Http\Controllers\BrandController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ColorController;
 // Route::get('/products', [ProductController::class, 'list']);
 // /api/v1/products
 Route::prefix('v1/products')->group(function () {
@@ -28,6 +29,14 @@ Route::prefix('v1/categories')->group(function () {
     Route::get('/{id}', [CategoryController::class, 'show']);     // GET    /api/categories/{id}
     Route::post('/', [CategoryController::class, 'store']);       // POST   /api/categories
     Route::put('/{id}', [CategoryController::class, 'update']);   // PUT    /api/categories/{id}
-    Route::delete('/{id}', [CategoryController::class, 'destroy']);// DELETE /api/categories/{id}
+    Route::delete('/{id}', [CategoryController::class, 'destroy']); // DELETE /api/categories/{id}
 });
 
+// routes/api.php
+Route::prefix('v1/colors')->group(function () {
+    Route::get('/', [ColorController::class, 'index']);
+    Route::get('/{id}', [ColorController::class, 'show']);
+    Route::post('/', [ColorController::class, 'store']);
+    Route::put('/{id}', [ColorController::class, 'update']);
+    Route::delete('/{id}', [ColorController::class, 'destroy']);
+});
