c47d85fcde61cd83d60317c0b8097df33fede5ad
HuyXinLoi <huyhodinh0909@gmail.com>
2025-04-05 10:41:56 +0700

    done carrts

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TestController;
 use App\Http\Controllers\BrandController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CartController;
 // Route::get('/products', [ProductController::class, 'list']);
 // /api/v1/products
 Route::prefix('v1/products')->group(function () {
@@ -29,5 +30,15 @@ Route::prefix('v1/categories')->group(function () {
     Route::post('/', [CategoryController::class, 'store']);       // POST   /api/categories
     Route::put('/{id}', [CategoryController::class, 'update']);   // PUT    /api/categories/{id}
     Route::delete('/{id}', [CategoryController::class, 'destroy']);// DELETE /api/categories/{id}
+});
+
+// Route::apiResource('v1/carts', CartController::class); // auto
+
+Route::prefix('v1/carts')->group(function () { // styles hehehe
+    Route::get('/', [CartController::class, 'index']);          // GET    /api/v1/carts
+    Route::get('/{id}', [CartController::class, 'show']);       // GET    /api/v1/carts/{id}
+    Route::post('/', [CartController::class, 'store']);         // POST   /api/v1/carts
+    Route::put('/{id}', [CartController::class, 'update']);     // PUT    /api/v1/carts/{id}
+    Route::delete('/{id}', [CartController::class, 'destroy']); // DELETE /api/v1/carts/{id}
 });
 
